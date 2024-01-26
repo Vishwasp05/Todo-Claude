cda6e52fcbd7238c4fa5f753cb62433d9061efaf
Vishwas Sharma <ch0nky@protonmail.com>
2024-01-26 20:18:45 +0530

    2.0 Input Validation added

---
+++ Todo-Claude/TodoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoDetailView.swift
+//  Todo-Claude
+//
+//  Created by Vishwas Sharma on 26/01/24.
+//
+
+import SwiftUI
+
+struct TodoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoDetailView()
+}
